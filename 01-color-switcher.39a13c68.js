f750b0040710f82856a18b7fda135d623015e378
DaryaFominaa <107459767+DaryaFominaa@users.noreply.github.com>
2022-11-18 17:20:56 +0000

    Deploying to gh-pages from @ DaryaFominaa/goit-js-hw-09@b168314b1c1944a91e45df13a9e2e9b3f859d79e 🚀

---
+++ 01-color-switcher.39a13c68.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");console.log(e);var n=null;t.addEventListener("click",(function(){n=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.39a13c68.js.map
