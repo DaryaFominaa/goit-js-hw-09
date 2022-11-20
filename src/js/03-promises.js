9d76794e1386c0f74b6b7b102a3567e56e7fe047
DaryaFominaa <fominadaryav@gmail.com>
2022-11-20 11:49:48 +0200

    add3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,41 +1,44 @@
 import Notiflix from 'notiflix';
-// console.log(Notiflix)
 
-const form = document.querySelector('.form')
+const form = document.querySelector('.form');
 form.addEventListener('submit', onSubmit);
-
-// console.log(form)
 
 function onSubmit(evt) {
   evt.preventDefault();
   const delay = evt.currentTarget.delay.value;
   const step = evt.currentTarget.step.value;
-   const amount = evt.currentTarget.amount.value;
-
-
+  const amount = evt.currentTarget.amount.value;
+  makeDelay(delay, step, amount);
 }
 
-
-
-
 function createPromise(position, delay) {
-
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
-    } else {
-      reject("Error! Error passed to reject function");
-    }
-  }, 2000);
-};
-
-
-
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
   });
+}
+
+function makeDelay(delay, step, amount) {
+  let stepDelay = delay;
+  for (let i = 1; i < amount; i += 1) {
+    // const element = array[i];
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
+
